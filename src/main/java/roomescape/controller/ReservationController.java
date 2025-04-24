ebef6879b2780d6f9b906cacefc423e9ed6cbc8e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-04-24 21:58:41 +0900

    refactor: Reservations 객체 제거

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,38 +2,36 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import roomescape.dao.ReservationDao;
 import roomescape.dto.ReservationRequest;
 import roomescape.reservation.Reservation;
-import roomescape.reservation.Reservations;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @RequestMapping("/reservations")
 @RestController
 public class ReservationController {
-    private final Reservations reservations;
+    private final ReservationDao reservationDao;
 
-    public ReservationController(Reservations reservations) {
-        this.reservations = reservations;
+    public ReservationController(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
     }
 
     @GetMapping
     public ResponseEntity<List<Reservation>> searchReservations() {
-        List<Reservation> findReservations = reservations.findAll();
+        List<Reservation> findReservations = reservationDao.findAll();
         return ResponseEntity.ok().body(findReservations);
     }
 
     @PostMapping
     public ResponseEntity<Reservation> addReservation(@RequestBody ReservationRequest reservationRequest) {
-        Long id = reservations.add(reservationRequest);
+        Long id = reservationDao.insert(reservationRequest);
         return ResponseEntity.ok().body(reservationRequest.toEntity(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        reservations.deleteById(id);
+        reservationDao.delete(id);
         return ResponseEntity.ok().build();
     }
 }
